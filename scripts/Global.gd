acb2f54b6f2ed96d06bf5cb04b39e40a74e3aed6
djalexkidd <alexpyr@outlook.fr>
2023-02-07 09:54:54 +0100

    Contrôle à la souris

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -1,3 +1,4 @@
 extends Node
 
 var credits = 0
+var mouse
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,6 +4,16 @@ var velocity = Vector2(0,0)
 const SPEED = 360
 var speed_multiplicator = 1
 
+func _ready():
+	if Global.mouse:
+		$Move.start()
+
+#Suit le mouvement de la souris sur l'axe X
+func _on_Move_timeout():
+	var tween = get_node("Tween")
+	tween.interpolate_property(self, "position",Vector2(position.x,position.y), Vector2(get_viewport().get_mouse_position().x,position.y),0.05 ,Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
+
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"): #Touche droite
 		velocity.x = SPEED * speed_multiplicator
